5c5e950fe2d8fd9faa3dec19acca3885ce958bed
sabarim6369 <sabarim6369@gmail.com>
2026-08-03 13:33:06 +0530

    Initialised required resources

--- identity-service/src/app.module.ts
+++ identity-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AddressesModule } from './addresses/addresses.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, AddressesModule],
   controllers: [AppController],
   providers: [AppService],
 })
