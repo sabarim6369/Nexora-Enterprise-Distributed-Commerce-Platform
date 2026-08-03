434770ab72f799d84dbfe6e9e1e27d0c539c5885
sabarim6369 <sabarim6369@gmail.com>
2026-08-03 14:39:05 +0530

    Degraded prisma to version 6

--- identity-service/src/prisma/prisma.service.ts
+++ identity-service/src/prisma/prisma.service.ts
@@ -1,20 +1,11 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
-
-  constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL!,
-    });
-
-    super({ adapter });
-  }
-
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
